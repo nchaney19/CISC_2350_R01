0cc7ad4d9f0a5376e23b0e2b763288476127fe89
nchaney19 <31755696+nchaney19@users.noreply.github.com>
2017-12-07 14:53:56 -0500

    Add files via upload

---
+++ final/Final/scripts/java2.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+var test = new pureJSLightBox({
+
+    // shows fullscreen overlay
+    overlay: false,
+
+    // shows navigation arrows
+    navigation: false,
+
+    // enable swipe
+    swipe: true
+    
+});
